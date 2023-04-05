fe82573f18039aa2fde0611d74854245185c298e
vanderleiacn <vanderleiacn@gmail.com>
2023-04-05 20:05:14 -0300

    Add project files.

---
+++ Principal.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SortearNumeroMegaSena
+{
+    partial class FrmMegaSena
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(231, 223);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(104, 43);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(209, 279);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(33, 20);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "......";
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(213, 324);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(0, 20);
+            this.lblContador.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(231, 342);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(8, 8);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // FrmMegaSena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnGerar);
+            this.Name = "FrmMegaSena";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mega Sena";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblContador;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Principal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortearNumeroMegaSena
+{
+    public partial class FrmMegaSena : Form
+    {
+        public FrmMegaSena()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            Sorteio();
+        }
+
+        private void Sorteio()
+        {
+            Random Sorteio = new Random();
+            int sorteado;
+            int[] verificar = new int[70];// esse 70 poderia ser qualquer numero desde que nao seja menor que o numero do array
+
+            string resultado = "";
+            for (int i = 0; i < 6; i++)
+            {
+            inicio: // goto 
+                sorteado = Sorteio.Next(0, 61);
+
+                for (int x = 0; x < 6; x++)//nesse caso usa o x pq o i ja esta sendo usado. usa-se apenas uma letra pra declarar uma variavel dentro da estrutura do for
+                {
+                    if (verificar[x] == sorteado)
+                    {
+                        goto inicio;//referencia para voltar para o inicio
+                    }
+                }
+
+
+                resultado = resultado + " " + sorteado;
+                lblResultado.Text = resultado;
+                verificar[i] = sorteado;
+            }
+
+        }
+    }
+}
